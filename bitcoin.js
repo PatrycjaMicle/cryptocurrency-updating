7b535a5592d2dd2f5164d3b07c01d039f2767f77
Pati <patrycja.starmach1993@gmail.com>
2023-01-28 00:10:57 +0100

    code optimize in js file

--- bitcoin.js
+++ bitcoin.js
@@ -3,7 +3,6 @@ document.getElementById("searchButton").addEventListener("click", function(){
     startLoading();
     stopLoading()
 });
-
 document.getElementById("coinName").addEventListener("keyup", function(event) {
     if (event.code === 'Enter'){
         event.preventDefault();
@@ -15,64 +14,47 @@ document.getElementById("coinName").addEventListener("keyup", function(event) {
 function startLoading() {
     document.body.style.cursor = "wait";
 }
-
 function stopLoading() {
     document.body.style.cursor = "default";
 }
-
 function getApi(url) {
     let daten;
     let datenJSON;
-
     function getData(x) {
         const request = new XMLHttpRequest();
         request.open("GET", x, false);
         request.send();
         return request.responseText;
     }
-
     datenJSON = getData(url);
     daten = JSON.parse(datenJSON);
     return daten;
-
 }
-
 function update(id) {
     return document.getElementById(id);
-
 }
-
 function coinDetails() {
-    var listUrl = "https://api.coingecko.com/api/v3/coins/list";
-    var coinList = getApi(listUrl);
 
-    function searchId() {
+    const listUrl = "https://api.coingecko.com/api/v3/coins/list";
+    const coinList = getApi(listUrl);
+    let inputLow = document.getElementById("coinName").value.toLowerCase();
+    let input = inputLow[0].toUpperCase() + inputLow.slice(1);
+    const coinId = coinList.find(coin => coin.name === input)?.id || null;
+    if(!coinId) return; //If coin is found, it accesses the id property of that object
+                        // and assigns it to the variable coinId.
+                        // If no match is found, coinId is assigned a value of null.
+                        // Then it check if coinId is null, if it is then it will return nothing.
 
-        let inputLow = document.getElementById("coinName").value.toLowerCase();
-        var input = inputLow[0].toUpperCase() + inputLow.slice(1);
-
-        for (let i = 0; i < coinList.length; i++) {
-            if (coinList[i].name === input) {
-                return coinList[i].id;
-            }
-        }
-        return null;
-
-    }
-
-    var coinId = searchId();
-    var coinUrl = "https://api.coingecko.com/api/v3/simple/price?ids=" + coinId + "&vs_currencies=eur&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true&precision=2"
-    var coinProperties = getApi(coinUrl);
-
-    var coin = coinProperties[coinId];
-    var price = coin.eur;
-    var change_24 = coin.eur_24h_change;
-    var volume_24 = coin.eur_24h_vol;
-    var mkt_cap = coin.eur_market_cap;
-
-    var url_ticker = "https://api.coingecko.com/api/v3/coins/" + coinId + "?tickers=true&market_data=false&community_data=false&developer_data=false&sparkline=true";
-    var ticker = getApi(url_ticker);
-    var logo_url = ticker.image.thumb;
+    const coinUrl = "https://api.coingecko.com/api/v3/simple/price?ids=" + coinId + "&vs_currencies=eur&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true&precision=2"
+    const coinProperties = getApi(coinUrl);
+    const coin = coinProperties[coinId];
+    const price = coin.eur;
+    const change_24 = coin.eur_24h_change;
+    const volume_24 = coin.eur_24h_vol;
+    const mkt_cap = coin.eur_market_cap;
+    const url_ticker = "https://api.coingecko.com/api/v3/coins/" + coinId + "?tickers=true&market_data=false&community_data=false&developer_data=false&sparkline=true";
+    const ticker = getApi(url_ticker);
+    const logo_url = ticker.image.thumb;
 
     update("coin_logo").src = logo_url;
     update("coin").innerHTML = coinId.toUpperCase();
